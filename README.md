9797ae2159015035577cce0ca140b19b5a451464
James Boynton <james.boynton0@gmail.com>
2021-06-10 01:20:41 -0400

    Initial plugin scaffolding

---
+++ README.md
@@ -0,0 +1,9 @@
+# Trinity Home Page v2
+
+Full-width home page design featuring a video banner.
+
+## Dependencies
+
+This plugin requires [Advanced Custom Fields Pro] to be installed and activated.
+
+[Advanced Custom Field Pro]: https://www.advancedcustomfields.com/pro
---
+++ trinity-home-page-v2.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Plugin Name: Trinity Home Page v2
+ * Plugin URI: https://github.com/xzito/trinity-home-page-v2
+ * Description: Full-width home page design featuring a video banner.
+ * Version: 0.1.0
+ * Author: James Boynton
+ */
+
+namespace Xzito\TrinityHomePageV2;
+
+$autoload_path = __DIR__ . '/vendor/autoload.php';
+
+if (file_exists($autoload_path)) {
+  require_once($autoload_path);
+}
+
+new TrinityHomePageV2();
